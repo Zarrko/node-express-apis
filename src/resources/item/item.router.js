aff2eb0debc1c7f8bd7497782db9d4101310b930
zarrko <zachariah.mwangi@imbank.co.ke>
2019-12-15 10:52:25 +0300

    feat(router): router & subrouter exercise

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { Router } from 'express'
+import controllers from './item.controllers'
 
 const router = Router()
+
+router
+    .route('/item')
+    .get(controllers.getOne)
+    .post(controllers.createOne)
+
+router
+    .route('/item/:id')
+    .get(controllers.getOne)
+    .put(controllers.updateOne)
+    .delete(controllers.removeOne)
 
 export default router
