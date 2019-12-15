9826ec40d26613dcb797a5a4d75c1a489edbac71
zarrko <zachariah.mwangi@imbank.co.ke>
2019-12-15 10:38:29 +0300

    feat(routing): routing & router verbs

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
@@ -7,9 +8,43 @@ export const app = express()
 
 app.disable('x-powered-by')
 
+// Middleware
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const router = express.Router()
+
+const log = (req, res, next) => {
+    console.log('Logging')
+    next()
+}
+
+router.get('/me', (req, res) => {
+    router.send({ message: 'me' })
+})
+
+app.use('/api', router)
+
+app.get('/', log, (req, res) => {
+    res.send({ message: 'hello' })
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send({ message: 'ok' })
+})
+export const start = () => {
+    app.listen(4000, () => {
+        console.log('Server is on 4000')
+    })
+}
+
+// router.route('/cat')
+//  .get()
+//  .post()
+// router.route('/cat/:id)
+//   .put()
+//   .delete()
+//   .get()
